f31a2848933cb6787c6d1980445ad0f8e75a3be6
Atilio Barreda <atiliobarreda@Mac.fios-router.home>
2025-03-06 15:46:15 -0500

    just frontend

--- thesis/viz/components/common/LoadingSpinner.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75">
-    <div class="w-12 h-12 border-4 border-gray-200 rounded-full animate-spin" style="border-top-color: #3b82f6"></div>
-  </div>
-</template> 
